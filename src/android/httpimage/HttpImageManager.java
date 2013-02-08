4d42b3605cbeeaf2732931565421d1a6d235ce5c
amineb <amine.bezzarga@labgency.com>
2013-02-08 18:39:28 +0100

    Target api correction

--- src/android/httpimage/HttpImageManager.java
+++ src/android/httpimage/HttpImageManager.java
@@ -22,6 +22,7 @@ import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.methods.HttpGet;
 
+import android.annotation.TargetApi;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -31,6 +32,7 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.TransitionDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Handler;
 import android.text.TextUtils;
 import android.util.Log;
@@ -663,6 +665,7 @@ public class HttpImageManager{
         }
     }
     
+	@TargetApi(Build.VERSION_CODES.DONUT)
 	private void setImageBitmapWithFade(final ImageView imageView, final Bitmap bitmap) {
 		Resources resources = imageView.getResources();
 		BitmapDrawable bitmapDrawable = new BitmapDrawable(resources,bitmap);
@@ -770,11 +773,13 @@ public class HttpImageManager{
 	    if (drawable instanceof BitmapDrawable) {
 	        return ((BitmapDrawable)drawable).getBitmap();
 	    }
-
-	    Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
-	    Canvas canvas = new Canvas(bitmap); 
-	    drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
-	    drawable.draw(canvas);
+	    Bitmap bitmap = null;
+	    if(drawable.getIntrinsicWidth()>0 && drawable.getIntrinsicHeight()>0){
+	    	bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
+	    	Canvas canvas = new Canvas(bitmap); 
+	    	drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+	    	drawable.draw(canvas);
+	    }
 
 	    return bitmap;
 	}
